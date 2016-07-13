7faf15547832904bdf2f9dc774da266316989dc1
fjmurray <fjmurray@live.com>
2016-07-12 21:17:05 -0400

    Update RecursiveSearch-1.py

--- RecursiveSearch-1.py
+++ RecursiveSearch-1.py
@@ -152,3 +152,12 @@ SyntaxError: 'return' outside function
 >>> s1
 'abc'
 >>> 
+
+function name: def isIn(char, aString):
+	'''
+	char: a single character
+    	aStr: an alphabetized string
+    
+    	returns: True if char is in aStr; False otherwise
+    	'''
+    # Your code here
